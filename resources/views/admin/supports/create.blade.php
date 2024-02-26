43561916d0532f24786a1287ac161a7fc64f72d3
paulomagnomiranda <paulo.miranda@softdesign.com.br>
2024-02-26 19:17:34 -0300

    feat: create StoreUpdateSupportRequest and apply validations in store method

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,14 +1,22 @@
 <h1>Nova Dúvida</h1>
 
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
 <form action="{{ route('supports.store') }}" method="post">
     @csrf
     <div>
         <label for="subject">Assunto</label>
-        <input type="text" name="subject" id="subject" placeholder="Assunto">
+        <input type="text" name="subject" id="subject" placeholder="Assunto" value="{{ old('subject') }}">
     </div>
     <div>
         <label for="body">Descrição</label>
-        <textarea name="body" id="body" cols="30" rows="5" placeholder="Descrição"></textarea>
+        <textarea name="body" id="body" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
     </div>
     <div>
         <button type="submit">Cadastrar</button>
