dca73cdc15a76f131cadc4e824b6977470d9c47a
paulomagnomiranda <paulo.miranda@softdesign.com.br>
2024-03-12 19:04:39 -0300

    feat: create component messages.blade and change file app.blade

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,17 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - {{ config('app.name') }}</title>
+
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <head>
-       @yield('header')
-    </head>
-    <div class="content">
-        @yield('content')
-    </div>
-    <footer>
-        Default Footer
-    </footer>
+    <section class="container px-4 mx-auto py-4">
+
+        @yield('header')
+
+        <div>
+            <x-messages/>
+            @yield('content')
+        </div>
+
+    </section>
 </body>
 </html>
