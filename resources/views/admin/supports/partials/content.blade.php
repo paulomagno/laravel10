2478eba31ba5af109101006b3062908317ad35eb
paulomagnomiranda <paulo.miranda@softdesign.com.br>
2024-03-13 16:20:29 -0300

    feat: install method of authentication

--- resources/views/admin/supports/partials/content.blade.php
+++ resources/views/admin/supports/partials/content.blade.php
@@ -1,39 +1,39 @@
 <div class="flex flex-col mt-6 my-4">
     <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
-                <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                    <thead class="bg-gray-50 dark:bg-gray-800">
+            <div class="overflow-hidden border border-gray-200 md:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
                         <tr>
-                            <th scope="col" class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <th scope="col" class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500">
                                 Assunto
                             </th>
 
-                            <th scope="col" class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <th scope="col" class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
                                 Status
                             </th>
 
-                            <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
                                 Comentário
                             </th>
 
-                            <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Interações</th>
+                            <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">Interações</th>
 
                             <th scope="col" class="relative py-3.5 px-4">
                                 <span class="sr-only">Ver</span>
                             </th>
                         </tr>
                     </thead>
-                    <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                    <tbody class="bg-white divide-y divide-gray-200">
                         @foreach ($supports->items() as $support)
                         <tr>
-                            <td class="px-4 py-2 text-sm font-medium whitespace-nowrap dark:text-white">
+                            <td class="px-4 py-2 text-sm font-medium whitespace-nowrap">
                                 {{ $support->subject }}
                             </td>
                             <td class="px-12 py-2 text-sm font-medium whitespace-nowrap">
                                 <x-status-support :status="$support->status"></x-status-support>
                             </td>
-                            <td class="px-4 py-2 text-sm whitespace-nowrap text-gray-500 dark:text-gray-400">
+                            <td class="px-4 py-2 text-sm whitespace-nowrap text-gray-500">
                                 {{ $support->body }}
                             </td>
                            
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,18 +20,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contact', [SiteController::class, 'contact']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+    Route::get('/contact', [SiteController::class, 'contact']);
 
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+    Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
-Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 
-Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 
-Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
 
-Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+
+    Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+
+    Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+});
+
+require __DIR__.'/auth.php';
